98bb27a83f1065dfe73a027c3aa10bbf6f76ec34
amy choi <cgc8016@ewhain.net>
2021-05-22 20:51:21 +0900

    style(TemplateRepository): remove basic offered methods by JpaRepository

--- src/main/java/com/cube/mailcube/domain/template/TemplateRepository.java
+++ src/main/java/com/cube/mailcube/domain/template/TemplateRepository.java
@@ -1,10 +1,8 @@
 package com.cube.mailcube.domain.template;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TemplateRepository extends JpaRepository<Template, Long> {
 	Optional<Template> findById(Long id);
-	List<Template> findAll();
 }
