f959bdb88f886374a6039a4496c79059d12fae04
amy choi <cgc8016@ewhain.net>
2021-05-22 19:34:27 +0900

    refactor(domain) : remove unused domains

--- src/main/java/com/cube/mailcube/domain/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cube.mailcube.domain;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	Optional<Employee> findById(Long id);
-}
--- src/main/java/com/cube/mailcube/domain/RecruitmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cube.mailcube.domain;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
-	Optional<Recruitment> findById(Long id);
-}
