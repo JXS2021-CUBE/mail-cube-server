6aa2ed485fb9a31df4318f40ce661a7b9f84dbb0
amy choi <cgc8016@ewhain.net>
2021-05-22 20:29:03 +0900

    fix(addTemplate): add request dto for request body in controller and service

--- src/main/java/com/cube/mailcube/controller/TemplateController.java
+++ src/main/java/com/cube/mailcube/controller/TemplateController.java
@@ -2,12 +2,14 @@ package com.cube.mailcube.controller;
 
 import com.cube.mailcube.domain.ErrorCase;
 import com.cube.mailcube.domain.ErrorMessage;
+import com.cube.mailcube.domain.template.TemplateRequestDto;
 import com.cube.mailcube.service.TemplateService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @AllArgsConstructor
@@ -15,12 +17,12 @@ public class TemplateController {
 	private final TemplateService templateService;
 
 	@PostMapping("/templates")
-	public ResponseEntity<Object> addTemplate(String title, String content) {
-		if (title == null || content == null) {
+	public ResponseEntity<Object> addTemplate(@RequestBody TemplateRequestDto templateRequestDto) {
+		if (templateRequestDto.getTitle() == null || templateRequestDto.getContent() == null) {
 			return ResponseEntity.badRequest()
 				.body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(),
 					ErrorCase.INVALID_FIELD_ERROR));
 		}
-		return ResponseEntity.ok().body(templateService.addTemplate(title, content));
+		return ResponseEntity.ok().body(templateService.addTemplate(templateRequestDto));
 	}
 }
--- src/main/java/com/cube/mailcube/service/TemplateService.java
+++ src/main/java/com/cube/mailcube/service/TemplateService.java
@@ -1,7 +1,8 @@
 package com.cube.mailcube.service;
 
-import com.cube.mailcube.domain.Template;
-import com.cube.mailcube.domain.TemplateRepository;
+import com.cube.mailcube.domain.template.Template;
+import com.cube.mailcube.domain.template.TemplateRepository;
+import com.cube.mailcube.domain.template.TemplateRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +13,11 @@ public class TemplateService {
 	private final TemplateRepository templateRepository;
 
 	@Transactional
-	public String addTemplate(String title, String content) {
+	public String addTemplate(TemplateRequestDto templateRequestDto) {
 		Template template = templateRepository.save(
 			Template.builder()
-			.name(title)
-			.content(content)
+			.name(templateRequestDto.getTitle())
+			.content(templateRequestDto.getContent())
 			.build());
 		return String.valueOf(template.getId());
 	}
