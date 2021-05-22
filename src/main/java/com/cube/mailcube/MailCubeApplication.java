a02ba19844a9408225628979b78d55543330203f
amy choi <cgc8016@ewhain.net>
2021-05-22 17:52:11 +0900

    feat: initial commit

---
+++ src/main/java/com/cube/mailcube/MailCubeApplication.java
@@ -0,0 +1,13 @@
+package com.cube.mailcube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailCubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailCubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cube/mailcube/MailCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cube.mailcube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailCubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
