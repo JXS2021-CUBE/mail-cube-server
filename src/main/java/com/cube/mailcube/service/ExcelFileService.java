0d0a20eabef6caccc33e7194e7022ee207cb740e
amy choi <cgc8016@ewhain.net>
2021-05-24 04:33:15 +0900

    fix(ExcelFileService): migration to aws

--- src/main/java/com/cube/mailcube/service/ExcelFileService.java
+++ src/main/java/com/cube/mailcube/service/ExcelFileService.java
@@ -3,7 +3,7 @@ package com.cube.mailcube.service;
 import com.cube.mailcube.domain.ApplicantDto;
 import com.cube.mailcube.domain.excelfile.ExcelFile;
 import com.cube.mailcube.domain.excelfile.ExcelFileRepository;
-import java.net.URI;
+import java.io.File;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.Timestamp;
@@ -17,41 +17,40 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 
 @Service
 @AllArgsConstructor
 public class ExcelFileService {
 
+	private final String EXCEL_FILE_PATH = "excel-file";
+
 	private final ExcelFileRepository excelfileRepository;
 	private final FileLoadService fileLoadService;
 
 	@Transactional
-	public String addExcelFile(MultipartFile multipartFile, String multipartName) {
+	public String addExcelFile(File convertedFile, String multipartName) {
 		Optional<ExcelFile> optionalExcelFile = excelfileRepository.findByName(multipartName);
+		String s3_url = fileLoadService.upload(convertedFile, EXCEL_FILE_PATH, multipartName);
+
 		if (optionalExcelFile.isPresent()) {
-			fileLoadService.fileUpload(multipartFile, multipartName);
 			return String.valueOf(optionalExcelFile.get().getId());
 		}
-
-		URI blob_url = fileLoadService.fileUpload(multipartFile, multipartName);
 
 		ExcelFile excelFile = excelfileRepository.save(
 			ExcelFile.builder()
 				.name(multipartName)
-				.blob_url(String.valueOf(blob_url))
+				.s3_url(s3_url)
 				.datetime(new Timestamp(System.currentTimeMillis()))
 				.build());
 		return String.valueOf(excelFile.getId());
 	}
 
-	public Optional<ExcelFile> getExcelFilebyId(Long id) {
+	public Optional<ExcelFile> getExcelFileById(Long id) {
 		return excelfileRepository.findById(id);
 	}
 
 	public List<ApplicantDto> getApplicants(String url) {
-
 		List<ApplicantDto> applicants = null;
 
 		try {
@@ -59,9 +58,6 @@ public class ExcelFileService {
 			URLConnection uc = urlAddress.openConnection();
 
 			XSSFWorkbook workbook = new XSSFWorkbook(uc.getInputStream());
-
-			int rowindex = 0;
-			int columnindex = 0;
 
 			XSSFSheet sheet = workbook.getSheetAt(0);
 			int rows = sheet.getPhysicalNumberOfRows();
@@ -73,11 +69,11 @@ public class ExcelFileService {
 				applicants.add(new ApplicantDto());
 			}
 
-			for (rowindex = 1; rowindex < rows; rowindex++) {
+			for (int rowindex = 1; rowindex < rows; rowindex++) {
 				XSSFRow row = sheet.getRow(rowindex);
 				if (row != null) {
 					int cells = row.getPhysicalNumberOfCells();
-					for (columnindex = 0; columnindex <= cells; columnindex++) {
+					for (int columnindex = 0; columnindex <= cells; columnindex++) {
 						XSSFCell cell = row.getCell(columnindex);
 						String value = "";
 						if (cell == null) {
@@ -100,19 +96,15 @@ public class ExcelFileService {
 							applicants.get(rowindex - 1).setEmail(value);
 						}
 					}
-
 				}
 			}
-
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.err.println(e.getMessage());
 		}
-
 		return applicants;
 	}
 
 	public List<ExcelFile> getAllExcelFiles() {
 		return excelfileRepository.findAll();
 	}
-
 }
