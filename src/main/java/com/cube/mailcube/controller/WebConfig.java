0a1b3d07664d71bb4aea5584a9a4415050187032
amy choi <cgc8016@ewhain.net>
2021-05-23 09:50:13 +0900

    feat(WebConfig): add CORS

--- src/main/java/com/cube/mailcube/controller/WebConfig.java
+++ src/main/java/com/cube/mailcube/controller/WebConfig.java
@@ -1,4 +1,4 @@
-package com.cube.mailcube.config;
+package com.cube.mailcube.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "https://mail-cube-web.vercel.app:80", "https://mailcube-chorom.vercel.app:80")
+			.allowedOrigins("http://localhost:3000", "https://mail-cube-web.vercel.app:80", "https://mailcube-chorom.vercel.app:80", "https://www.test-cors.org:80")
 			.allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
 			.exposedHeaders("Location");
 	}
