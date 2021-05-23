76b45a526b6587bc9f72e7f4da9f6d010ea25498
amy choi <cgc8016@ewhain.net>
2021-05-23 10:00:27 +0900

    fix(WebConfig)

--- src/main/java/com/cube/mailcube/config/WebConfig.java
+++ src/main/java/com/cube/mailcube/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.cube.mailcube.controller;
+package com.cube.mailcube.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
